ca88b2e03e32de725105756779eabe82f250a6c0
Kunal Keshan <kunalkeshan12@gmail.com>
2024-05-04 11:43:26 +0530

    chore: remove unused schmeas (temp)

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,11 +1,15 @@
 import { type SchemaTypeDefinition } from 'sanity';
 
 import blockContent from './schemaTypes/blockContent';
-import category from './schemaTypes/category';
-import post from './schemaTypes/post';
-import author from './schemaTypes/author';
+// import category from './schemaTypes/category';
+// import post from './schemaTypes/post';
+// import author from './schemaTypes/author';
 import research from './schemaTypes/research';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-	types: [post, author, category, blockContent, research],
+	types: [
+		// post, author, category,
+		blockContent,
+		research,
+	],
 };
