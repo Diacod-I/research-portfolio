cd3ff59ab70e13f7f2446b5fb726261e1f5fd22a
Luci <98768417+Diacod-I@users.noreply.github.com>
2024-05-05 17:32:17 +0530

    chore: Changed the card

--- src/app/(static)/research/[slug]/page.tsx
+++ src/app/(static)/research/[slug]/page.tsx
@@ -49,8 +49,6 @@ const IndividualResearch = async ({ params }: { params: QueryParams }) => {
 	}
 	return (
 		<div className='w-full max-w-3xl mx-auto p-4 lg:p-8'>
-			<h1 className='text-lg font-semibold'>{research.title}</h1>
-			<h2 className='mt-4'>{research.description}</h2>
 			<ResearchBody body={research.body} />
 		</div>
 	);
